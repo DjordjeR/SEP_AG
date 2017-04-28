2f2b47a98c30c6b6ea6894212a965aa12ab05171
djordjer <rajicdj@gmail.com>
2017-04-28 10:54:34 +0200

    Student avg

---
+++ student_avg/code/Student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DjordjeR on 28.04.17.
+//
+
+#include "Student.h"
---
+++ student_avg/code/Student.h
@@ -0,0 +1,15 @@
+//
+// Created by DjordjeR on 28.04.17.
+//
+
+#ifndef SEP_AG_STUDENT_H
+#define SEP_AG_STUDENT_H
+
+
+class Student
+{
+
+};
+
+
+#endif //SEP_AG_STUDENT_H
